ba62cdfa96d67aec47d4d57d9e350dc899f9d272
chiragvaswani <chiragvaswani751@gmail.com>
2021-05-26 19:42:54 +0530

    Define constructor and dynamic arrays that store the accounts of each bank

--- Bank.java
+++ Bank.java
@@ -1,3 +1,12 @@
+import java.util.*;
+
 public class Bank {
+    private ArrayList<Account> A;
+    private ArrayList<Account> B;
+
+    public Bank() {
+        A = new ArrayList<>();
+        B = new ArrayList<>();
+    }
 
 }
