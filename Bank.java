8e4ed968c270943e6fe6ffbb5cfc147ae194afb2
chiragvaswani <chiragvaswani751@gmail.com>
2021-05-26 19:46:08 +0530

    Define getter method that prints the details of all the accounts

--- Bank.java
+++ Bank.java
@@ -16,4 +16,13 @@ public class Bank {
     public void addBankB(Account account) {
         B.add(account);
     }
+
+    public void showAccounts() {
+        System.out.println("Accounts in Bank A: ");
+        for (Account account : A)
+            System.out.println(account.toString());
+        System.out.println("Accounts in Bank B: ");
+        for (Account account : B)
+            System.out.println(account.toString());
+    }
 }
