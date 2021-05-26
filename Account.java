cf7ef98c037958043e63fd8432f2c7164bdd3fd8
chiragvaswani <chiragvaswani751@gmail.com>
2021-05-26 23:31:41 +0530

    Define regular and parameterised constructors

--- Account.java
+++ Account.java
@@ -1,3 +1,27 @@
-public class Account {
+public class Account extends Bank {
+    private String name;
+    private double balance;
+    private int card;
+    private String exp;
+    private String pass;
+
+    Account() {
+        super();
+        this.name = "";
+        this.balance = 40;
+        this.card = 0;
+        this.exp = "";
+        this.exp = "";
+        this.pass = "";
+    }
+
+    Account(String name, double balance, int card, String exp, String pass) {
+        this.name = name;
+        this.balance = balance;
+        this.card = card;
+        this.exp = exp;
+        this.pass = pass;
+
+    }
 
 }
